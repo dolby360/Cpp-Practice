4a97f2a759059052b4880cca4973d8c8708944c6
dolev.ben-aharon <dolev.ben-aharon@hp.com>
2019-06-06 18:32:15 +0300

    isAnagram

--- Level_7/Anagram.hpp
+++ Level_7/Anagram.hpp
@@ -1,34 +1,6 @@
 #include <string>
-#include <iostream>
 #include <algorithm>
 using namespace std;
-
-// string getCharsInRangeAndOrderd(const string str,const char a,const char b){
-//   string temp;
-//   for(int i = 0; i < str.size(); i++){
-//     if(str[i] >= a && str[i] <= b){
-//       temp += str[i];
-//     }
-//   }
-//   std::sort(temp.begin(), temp.end());
-//   cout << "************" << endl;
-//   cout << str << endl;
-//   cout << temp << endl;
-//   cout << "************" << endl;
-//   return temp;
-// }
-
-
-// bool isAnagram(std::string test, std::string original){
-//   cout << endl;
-//   if(getCharsInRangeAndOrderd(test,'a','z') !=   getCharsInRangeAndOrderd(original,'a','z')){
-//     return false;
-//   }
-//   if(getCharsInRangeAndOrderd(test,'A','Z') !=   getCharsInRangeAndOrderd(original,'A','Z')){
-//     return false;
-//   }
-//   return true;
-// }
 
 
 bool isAnagram(std::string test, std::string original){
